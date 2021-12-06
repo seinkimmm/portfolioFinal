9cf71bf4eafa6c5e8801971627c996022a609105
Sein Kim <55934737+seinkimmm@users.noreply.github.com>
2021-12-06 12:21:24 -0500

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,13 @@
+
+// alert("Hello! I am an alert box!!");
+// var parent = document.getElementById("hello");
+// document.getElementById("hello").style.opacity = 1;
+// parent.onfocus = function() {
+//     document.getElementById("hello").focus()
+// }
+setTimeout(function () {
+    document.getElementById('homeGIF').style.display='none';
+}, 4000);
+
+
+
